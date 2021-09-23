c2fc4f784838154bfd6d6bfa5b63d728b1334a78
junkaitongxue <252647920@qq.com>
2021-09-24 00:02:04 +0800

    【describe】enhance demo

--- demo/src/main/java/com/dreamkite/demo/controller/AController.java
+++ demo/src/main/java/com/dreamkite/demo/controller/AController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AController {
 
+
+    /**
+     * 浏览器输入：http://localhost:8080/hi
+     *
+     * @return hi
+     */
     @GetMapping("/hi")
     public String hi() {
         return "hi, springboot.";
