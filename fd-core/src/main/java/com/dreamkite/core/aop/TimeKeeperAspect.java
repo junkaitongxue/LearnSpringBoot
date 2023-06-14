45178140cda98380d0f9f805826a921f599dc55d
junkaitongxue <252647920@qq.com>
2023-06-15 00:09:56 +0800

    【describe】增强类和函数的切面处理逻辑

--- fd-core/src/main/java/com/dreamkite/core/aop/TimeKeeperAspect.java
+++ fd-core/src/main/java/com/dreamkite/core/aop/TimeKeeperAspect.java
@@ -7,13 +7,17 @@ import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
-
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+/**
+ * 启动该计时器需要添加配置：timeKeeper.enable=true
+ */
 @Slf4j
 @Component
 @Aspect
+@ConditionalOnProperty(name = "timeKeeper.enable")
 public class TimeKeeperAspect {
     @Around("@within(com.dreamkite.core.aop.TimeKeeper) && !@annotation(com.dreamkite.core.aop.TimeKeeper)")
     public Object keepForClass(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
--- fd-core/src/test/java/com/dreamkite/core/aop/TimeKeeperAspectTest.java
+++ fd-core/src/test/java/com/dreamkite/core/aop/TimeKeeperAspectTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import javax.annotation.Resource;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class TimeKeeperAspectTest extends CommonSpringTest {
 
